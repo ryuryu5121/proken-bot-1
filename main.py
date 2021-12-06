a54d574744810e64e782987828e64486f2a5f95b
hibiki1228 <devkimadore1228@gmail.com>
2021-12-06 17:14:54 +0900

    add test code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage
+    MessageEvent, TextMessage, TextSendMessage, QuickReplyButton, MessageAction, QuickReply,ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage
 )
 import os
 import random
@@ -20,6 +20,14 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+lesson = {
+    '月曜日の時間割':'月曜日は\n1:こくご\n2:たいいく\n3:さんすう\nです。',
+    '火曜日の時間割':'火曜日は\n1~2:りかじっけん\n3:こくご\nです。',
+    '水曜日の時間割':'水曜日は\n1:さんすう\n2:おんがく\n3:せいかつ\nです。',
+    '木曜日の時間割':'木曜日は\n1:たいいく\n2:かていか\n3:りか\nです。',
+    '金曜日の時間割':'金曜日は\n1:さんすう\n2:こくご\n3:がっかつ\nです。'
+}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -46,6 +54,18 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=message))
+
+def handle_message(event):
+    if event.message.text =='時間割を教えて':
+        day_list = ["月", "火", "水", "木", "金"]
+        items = [QuickReplyButton(action=MessageAction(label=f"{day}", text=f"{day}曜日の時間割")) for day in day_list]
+        messages = TextSendMessage(text="何曜日の時間割ですか？",quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token, messages=messages)
+    elif event.message.text in lesson:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text=lesson[event.message.text])
+        )
 
 
 if __name__ == "__main__":
