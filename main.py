742b6ca4d7aaaa2c3be5369c0e4900acfa9ef3cc
hibiki1228 <devkimadore1228@gmail.com>
2021-12-07 18:18:50 +0900

    change

--- main.py
+++ main.py
@@ -48,24 +48,24 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     # 基本的にここにコードを書いていきます。
-#     message = event.message.text
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=message))
-
 def handle_message(event):
-    if event.message.text =='時間割を教えて':
-        day_list = ["月", "火", "水", "木", "金"]
-        items = [QuickReplyButton(action=MessageAction(label=f"{day}", text=f"{day}曜日の時間割")) for day in day_list]
-        messages = TextSendMessage(text="何曜日の時間割ですか？",quick_reply=QuickReply(items=items))
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-    elif event.message.text in lesson:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextMessage(text=lesson[event.message.text])
-        )
+    # 基本的にここにコードを書いていきます。
+    message = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=message))
+
+# def handle_message(event):
+#     if event.message.text =='時間割を教えて':
+#         day_list = ["月", "火", "水", "木", "金"]
+#         items = [QuickReplyButton(action=MessageAction(label=f"{day}", text=f"{day}曜日の時間割")) for day in day_list]
+#         messages = TextSendMessage(text="何曜日の時間割ですか？",quick_reply=QuickReply(items=items))
+#         line_bot_api.reply_message(event.reply_token, messages=messages)
+#     elif event.message.text in lesson:
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextMessage(text=lesson[event.message.text])
+#         )
 
 
 if __name__ == "__main__":
